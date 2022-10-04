f67f52de9ef8de94629fd4c3693394137be3f19e
cristianms1009 <litkillah1009@gmail.com>
2022-10-04 15:12:47 -0300

    agregamos npm circule progress

--- Integrador/src/app/app.module.ts
+++ Integrador/src/app/app.module.ts
@@ -10,6 +10,11 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HysComponent } from './components/hys/hys.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +25,15 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     ExperienciaComponent,
     ProyectosComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    FooterComponent,
+    HysComponent,
+    EducacionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
